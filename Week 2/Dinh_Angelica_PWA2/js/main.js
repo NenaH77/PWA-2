ee976238a5525b22df6d425579074ace1d3eb25a
Angelica <hernandez.angelica77@gmail.com>
2014-06-11 02:01:41 -0400

    updates for wk2

--- Week 2/Dinh_Angelica_PWA2/js/main.js
+++ Week 2/Dinh_Angelica_PWA2/js/main.js
@@ -6,7 +6,7 @@
 (function($){
 	
 
-	/* ============= Add Modal ======================= */
+	/* ============= Add Modal ======================================= */
 
     $('.modalClick').on('click', function(e){
         e.preventDefault();
@@ -33,6 +33,29 @@
     $('.mystatus').mouseout(function(){
         $(this).fadeTo(100, 1);
     });
+
+    /* ============= toolTip ====================================== */
+
+    $('.masterTooltip').hover(function(){
+        //Hover over code
+        var title = $(this).attr('title');
+        $(this).data('tipText', title).removeAttr('title');
+        $('<p class="tooltip"></p>')
+            .text(title)
+            .appendTo('body')
+            .fadeIn('slow');
+    }, function(){
+            //Hover out code
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+        }) .mousemove(function(e){
+            var mousex = e.pageX + 20;//get X coordinates
+            var mousey = e.pageY + 10;//get y coordinates
+            $('.tooltip').css({ top: mousey, left: mousex })
+        });
+
+
+
 
 	/*
 	===============================================
